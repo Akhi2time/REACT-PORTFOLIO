595409dc7c2a535a103fd444cbfc7f5f6e89f3d1
Mohamed <m.mohamedsandiego@gmail.com>
2022-11-26 17:31:54 -0600

    Footers,Contacts

---
+++ name-of-app/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Footer(){
+    return(
+        <div class="footer" id="footer">
+            <a href="https://github.com/Akhi2time"> alt="Github" </a>
+            <a href="https://www.linkedin.com/feed/"> alt="LinkedIn" </a>
+            <a href="https://stackoverflow.com/users/20610202/mohamed-mohamed"> alt="Stackoverflow" </a>
+        </div>
+    )
+}
+
+
+
+export default Footer;
