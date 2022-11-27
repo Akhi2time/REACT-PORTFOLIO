0633d57ee6dcfb3dd889857197030d0cf2b57efd
Mohamed <m.mohamedsandiego@gmail.com>
2022-11-26 20:24:23 -0600

    Footer Work

--- name-of-app/src/components/Footer.js
+++ name-of-app/src/components/Footer.js
@@ -3,9 +3,9 @@ import React from 'react';
 function Footer(){
     return(
         <div class="footer" id="footer">
-            <a href="https://github.com/Akhi2time"> Github </a>
-            <a href="https://www.linkedin.com/feed/"> LinkedIn </a>
-            <a href="https://stackoverflow.com/users/20610202/mohamed-mohamed"> Stackoverflow </a>
+            <a href="https://github.com/Akhi2time"><img src="https://img.icons8.com/color/48/000000/github-2.png" alt="Github" className="icon"/></a>
+            <a href="https://www.linkedin.com/feed/"><img src="https://img.icons8.com/color/48/000000/linkedin.png" alt="LinkedIn" className="icon"/></a>
+            <a href="https://stackoverflow.com/users/20610202/mohamed-mohamed">
         </div>
     )
 }
