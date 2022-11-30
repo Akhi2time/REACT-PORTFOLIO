cdf6465a21cdd8cfd09ab1872023e21dececd9c1
Mohamed <m.mohamedsandiego@gmail.com>
2022-11-30 17:23:12 -0600

    6 Projects

--- name-of-app/src/components/Footer.js
+++ name-of-app/src/components/Footer.js
@@ -5,7 +5,7 @@ function Footer(){
         <div class="footer" id="footer">
             <a href="https://github.com/Akhi2time"><img src="https://img.icons8.com/color/48/000000/github-2.png" alt="Github" className="icon"/></a>
             <a href="https://www.linkedin.com/feed/"><img src="https://img.icons8.com/color/48/000000/linkedin.png" alt="LinkedIn" className="icon"/></a>
-            <a href="https://stackoverflow.com/users/20610202/mohamed-mohamed">
+            <a href="https://stackoverflow.com/users/20610202/mohamed-mohamed"><img src="https://img.icons8.com/color/48/000000/stackoverflow.png" alt="Stack Overflow" className="icon"/></a>
         </div>
     )
 }
