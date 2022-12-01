af8ca011d5438b34ea7df0de17b7f97cbfb714df
Mohamed <m.mohamedsandiego@gmail.com>
2022-11-30 18:07:17 -0600

    Updated Link

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,10 @@ return (
     <div>
       <h1>About ME</h1>
       <p>
-        Hi, My name is Mohamed Mohamed. I am from San Diego, CA moved out here to Minnesota early this year. I am an aspiring Software Engineer and Full Stack Web Developer.
+        Hi, 
+        My name is Mohamed Mohamed. 
+        I am from San Diego, CA moved out here to Minnesota early this year. 
+        I am an aspiring Software Engineer and Full Stack Web Developer.
       </p>
     </div>
   );
